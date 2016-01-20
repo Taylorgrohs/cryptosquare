c976cdf03305471976ab2526773cf8cde88f2a43
Jay Whang and Taylor Grohs <jaywhang83@gmail.com>
2016-01-19 16:21:25 -0800

    add final touches to javascript sorter

--- spec/specs.js
+++ spec/specs.js
@@ -10,12 +10,13 @@ var test = "don't compare yourself to others, compare yourself to the person you
 test = test.split("");
 var set = [];
 var set2 = [];
+var empty;
 var row = 8;
 var column = 9;
 var count = 0;
 
-var row2 = 5;
-var col = 14;
+var row2 = 8;
+var col = 10;
 
 for ( var i = 0; i < row; i++) {
   set[i] = new Array();
@@ -30,19 +31,11 @@ for ( var i = 0; i < row; i++) {
 for ( var i = 0; i < col; i++) {
   set2[i] = new Array();
   for (var j = 0; j < row2; j++) {
-    set2[i].push(set[0][0]);
-    set2[i].push(set[1][0]);
-    set2[i].push(set[2][0]);
-    set2[i].push(set[3][0]);
-    set2[i].push(set[4][0]);
-    set[0].splice(0,1);
-    set[1].splice(0,1);
-    set[2].splice(0,1);
-    set[3].splice(0,1);
+    set2[i][j] = set[j][i];
+    
 }
 }
+set2.toString();
+//set2[i].push(set[j][0]);
+//set[j].splice(0,1);
     //set2[i].push(set[j+9]);
-
-    //set2[i][j] = set[j][i];
-  }
-}
